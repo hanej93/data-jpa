ed68627fe6dc9b7d0039d36f8dc42bad8a95d9c5
EJ <hanej93@naver.com>
2023-02-06 20:28:12 +0900

    사용자 정의 리포지토리 구현

---
+++ src/main/java/study/datajpa/repository/MemberRepositoryImpl.java
@@ -0,0 +1,20 @@
+package study.datajpa.repository;
+
+import lombok.RequiredArgsConstructor;
+import study.datajpa.entity.Member;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class MemberRepositoryImpl implements MemberRepositoryCustom {
+
+    private final EntityManager em;
+
+    @Override
+    public List<Member> findMemberCustom() {
+        String sql = "select m from Member m";
+        return em.createQuery(sql, Member.class)
+                .getResultList();
+    }
+}
